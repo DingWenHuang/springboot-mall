e1c2ebfc6d4a179b87fb0785e2775c2e63e93b10
Wen <test@email.com>
2023-06-07 14:14:59 +0800

    product feature - queryProductList by category and keyword implementation

--- src/main/java/com/wenhuang/springbootmall/service/ProductService.java
+++ src/main/java/com/wenhuang/springbootmall/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.wenhuang.springbootmall.service;
 
+import com.wenhuang.springbootmall.constant.ProductCategory;
 import com.wenhuang.springbootmall.dto.ProductRequest;
 import com.wenhuang.springbootmall.model.Product;
 
@@ -7,7 +8,7 @@ import java.util.List;
 
 public interface ProductService {
 
-    List<Product> getProducts();
+    List<Product> getProducts(ProductCategory category, String search);
 
     Product getProductById(Integer productId);
 
