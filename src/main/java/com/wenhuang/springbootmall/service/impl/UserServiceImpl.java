8eec783537907599fdc75c000125e9eaf7dedd54
Wen <test@email.com>
2023-06-08 14:36:42 +0800

    account feature - check email and hide password

--- src/main/java/com/wenhuang/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/wenhuang/springbootmall/service/impl/UserServiceImpl.java
@@ -4,22 +4,37 @@ import com.wenhuang.springbootmall.dao.UserDao;
 import com.wenhuang.springbootmall.dto.UserRegisterRequest;
 import com.wenhuang.springbootmall.model.User;
 import com.wenhuang.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserDao userDao;
 
     @Override
-    public Integer register(UserRegisterRequest userRegisterRequest) {
-        return userDao.createUser(userRegisterRequest);
+    public User getUserById(Integer userId) {
+        return userDao.getUserById(userId);
     }
 
     @Override
-    public User getUserById(Integer userId) {
-        return userDao.getUserById(userId);
+    public Integer register(UserRegisterRequest userRegisterRequest) {
+        //check if email has already been registered or not
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if (user != null) {
+            log.warn("email {} has already been registered!", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        //create a new user
+        return userDao.createUser(userRegisterRequest);
     }
 }
