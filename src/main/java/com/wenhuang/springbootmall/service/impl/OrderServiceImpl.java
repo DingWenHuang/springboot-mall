b63988c4520d49c812d3e32bf3064cf8eb231054
Wen <test@email.com>
2023-06-09 11:03:35 +0800

    orderList feature - check productId and stock

--- src/main/java/com/wenhuang/springbootmall/service/impl/OrderServiceImpl.java
+++ src/main/java/com/wenhuang/springbootmall/service/impl/OrderServiceImpl.java
@@ -2,15 +2,21 @@ package com.wenhuang.springbootmall.service.impl;
 
 import com.wenhuang.springbootmall.dao.OrderDao;
 import com.wenhuang.springbootmall.dao.ProductDao;
+import com.wenhuang.springbootmall.dao.UserDao;
 import com.wenhuang.springbootmall.dto.BuyItem;
 import com.wenhuang.springbootmall.dto.CreateOrderRequest;
 import com.wenhuang.springbootmall.model.Order;
 import com.wenhuang.springbootmall.model.OrderItem;
 import com.wenhuang.springbootmall.model.Product;
+import com.wenhuang.springbootmall.model.User;
 import com.wenhuang.springbootmall.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,11 +24,16 @@ import java.util.List;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+    private final static Logger log = LoggerFactory.getLogger(OrderServiceImpl.class);
+
     @Autowired
     private OrderDao orderDao;
 
     @Autowired
     private ProductDao productDao;
+
+    @Autowired
+    private UserDao userDao;
 
     @Override
     public Order getOrderById(Integer orderId) {
@@ -38,11 +49,32 @@ public class OrderServiceImpl implements OrderService {
     @Transactional
     @Override
     public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
+        // check if userId exist or not
+        User user = userDao.getUserById(userId);
+
+        if (user == null) {
+            log.warn("this user ID : {} is not exist!", userId);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+
         int totalAmount = 0;
         List<OrderItem> orderItemList = new ArrayList<>();
 
         for (BuyItem buyItem : createOrderRequest.getBuyItemList()) {
             Product product = productDao.getProductById(buyItem.getProductId());
+
+            // check if product is exist or not and product stock is sufficient or not
+            if (product == null) {
+                log.warn("this product : {} is not exist!", buyItem.getProductId());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            } else if (product.getStock() < buyItem.getQuantity()) {
+                log.warn("product : {} stock is not sufficient! Stock : {}, Quantity to buy : {}",
+                        buyItem.getProductId(), product.getStock(), buyItem.getQuantity());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+
+            productDao.updateStock(product.getProductId(), product.getStock() - buyItem.getQuantity());
 
             //calculate total amount
             int amount = buyItem.getQuantity() * product.getPrice();
